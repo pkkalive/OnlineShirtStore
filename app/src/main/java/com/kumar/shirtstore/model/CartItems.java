ad9d8657a12ee3656df818905ed783c4c1e7f37c
Kalive Purushotham Kumar <pkkalive@users.noreply.github.com>
2017-08-07 16:41:17 +0530

    refactor the cartItems class

---
+++ app/src/main/java/com/kumar/shirtstore/model/CartItems.java
@@ -0,0 +1,85 @@
+package com.kumar.shirtstore.model;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by Purushotham on 06/08/17.
+ */
+
+public class CartItems {
+
+    private Integer id;
+    private Integer price;
+    private String picture;
+    private String colour;
+    private String size;
+    private String name;
+    private Integer quantity;
+    private Map<String, Object> additionalProperties = new HashMap<String, Object>();
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getPrice() {
+        return price;
+    }
+
+    public void setPrice(Integer price) {
+        this.price = price;
+    }
+
+    public String getPicture() {
+        return picture;
+    }
+
+    public void setPicture(String picture) {
+        this.picture = picture;
+    }
+
+    public String getColour() {
+        return colour;
+    }
+
+    public void setColour(String colour) {
+        this.colour = colour;
+    }
+
+    public String getSize() {
+        return size;
+    }
+
+    public void setSize(String size) {
+        this.size = size;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(Integer quantity) {
+        this.quantity = quantity;
+    }
+
+    public Map<String, Object> getAdditionalProperties() {
+        return this.additionalProperties;
+    }
+
+    public void setAdditionalProperty(String name, Object value) {
+        this.additionalProperties.put(name, value);
+    }
+
+}
