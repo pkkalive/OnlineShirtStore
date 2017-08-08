6b4e28a46b25d8ae378510eda343d8e88bb53250
Kalive Purushotham Kumar <pkkalive@users.noreply.github.com>
2017-08-08 18:46:53 +0530

    added functionalities to image cache manager

--- app/src/main/java/com/kumar/shirtstore/utils/ImageCacheManager.java
+++ app/src/main/java/com/kumar/shirtstore/utils/ImageCacheManager.java
@@ -1,8 +1,54 @@
 package com.kumar.shirtstore.utils;
 
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+
+import com.kumar.shirtstore.model.CartItems;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
 /**
  * Created by Purushotham on 08/08/17.
  */
 
 public class ImageCacheManager {
+    public static Bitmap getBitmap(Context context, CartItems cartItems) {
+
+        String fileName = context.getCacheDir() + "/" + cartItems.getPicture();
+        File file = new File(fileName);
+        if (file.exists()) {
+            try {
+                return BitmapFactory.decodeStream(new FileInputStream(file));
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            }
+        }
+        return null;
+
+    }
+
+    public static void putBitmap(Context context, CartItems cartItems, Bitmap bitmap) {
+        String fileName = context.getCacheDir() + "/" + cartItems.getPicture();
+        File file = new File(fileName);
+        FileOutputStream outputStream = null;
+        try {
+            outputStream = new FileOutputStream(file);
+            bitmap.compress(Bitmap.CompressFormat.JPEG, 85, outputStream);
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (outputStream != null) {
+                try {
+                    outputStream.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
 }
