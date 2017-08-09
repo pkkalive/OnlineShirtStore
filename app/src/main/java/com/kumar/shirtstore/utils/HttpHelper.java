9664f0b0c6337eebf6be789b762df179d038d821
Kalive Purushotham Kumar <pkkalive@users.noreply.github.com>
2017-08-09 07:09:44 +0530

    implemented both GET and POST request using OkHTTP

--- app/src/main/java/com/kumar/shirtstore/utils/HttpHelper.java
+++ app/src/main/java/com/kumar/shirtstore/utils/HttpHelper.java
@@ -14,9 +14,12 @@ import java.io.InputStream;
 import java.io.OutputStreamWriter;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.Map;
 
+import okhttp3.MultipartBody;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
+import okhttp3.RequestBody;
 import okhttp3.Response;
 
 /**
@@ -50,6 +53,17 @@ public class HttpHelper {
         OkHttpClient client = new OkHttpClient();
         Request.Builder requestBuilder = new Request.Builder()
                 .url(address);
+
+        if (requestPackage.getMethod().equals("POST")) {
+            MultipartBody.Builder builder = new MultipartBody.Builder()
+                    .setType(MultipartBody.FORM);
+            Map<String, String> params = requestPackage.getParams();
+            for (String key : params.keySet()) {
+                builder.addFormDataPart(key, params.get(key));
+            }
+            RequestBody requestBody = builder.build();
+            requestBuilder.method("POST", requestBody);
+        }
 
         Request request = requestBuilder.build();
         Response response = client.newCall(request).execute();
